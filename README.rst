1e22fcc8681cd14391c2343c81aa6c533a9bc092
lsaltz <theelleris13x@gmail.com>
2024-08-29 10:26:13 -0600

    initial

---
+++ README.rst
@@ -0,0 +1,9 @@
+Assignment for OS class at OSU. See final report for details.
+
+Modified files:
+
+* ``builtins.c``
+* ``signal.c``
+* ``vars.c``
+* ``runner.c``
+* ``wait.c``
---
+++ src/signal.c
@@ -0,0 +1,106 @@
+#define _POSIX_C_SOURCE 200809L
+#include <signal.h>
+#include <errno.h>
+#include <stddef.h>
+
+#include "signal.h"
+
+//https://www.ibm.com/docs/en/zos/3.1.0?topic=functions-sigaction-examine-change-signal-action
+// https://pubs.opengroup.org/onlinepubs/009695399/functions/sigaction.html
+
+static void
+interrupting_signal_handler(int signo)
+{
+  /* Its only job is to interrupt system calls--like read()--when
+   * certain signals arrive--like Ctrl-C.
+   */
+}
+
+static struct sigaction ignore_action = {.sa_handler = SIG_IGN},
+                        interrupt_action = {.sa_handler =
+                                                interrupting_signal_handler},
+                        old_sigtstp, old_sigint, old_sigttou;
+
+/* Ignore certain signals.
+ * 
+ * @returns 0 on succes, -1 on failure
+ *
+ *
+ * The list of signals to ignore:
+ *   - SIGTSTP  ^Z
+ *   - SIGINT ^C
+ *   - SIGTTOU Reading or Writing from background
+ *
+ * Should be called immediately on entry to main() 
+ *
+ * Saves old signal dispositions for a later call to signal_restore()
+ */
+int
+signal_init(void)
+{
+  /* TODO Initialize signals, store old actions 
+   *
+   * e.g. sigaction(SIGNUM, &new_handler, &saved_old_handler);
+   *
+   * */
+  // Change action to ignore, saves previous action
+  if (sigaction(SIGTSTP, &ignore_action, &old_sigtstp) == -1) return -1; 
+  if (sigaction(SIGINT, &ignore_action, &old_sigint) == -1) return -1;
+  if (sigaction(SIGTTOU, &ignore_action, &old_sigttou) == -1) return -1;
+  //errno = ENOSYS; /* not implemented */
+  return 0;
+}
+
+/** enable signal to interrupt blocking syscalls (read/getline, etc) 
+ *
+ * @returns 0 on succes, -1 on failure
+ *
+ * does not save old signal disposition
+ */
+int
+signal_enable_interrupt(int sig)
+{
+  /* TODO set the signal disposition for signal to interrupt  */
+  // Change action without storing the old action
+  if (sigaction(sig, &interrupt_action, NULL) < 0) return -1;
+  return 0;
+}
+
+/** ignore a signal
+ *
+ * @returns 0 on success, -1 on failure
+ *
+ * does not save old signal disposition
+ */
+int
+signal_ignore(int sig)
+{
+  /* TODO set the signal disposition for signal back to its old state */
+  // Change to ignore
+  if (sigaction(sig, &ignore_action, NULL)<0) return -1;
+  return 0;
+  //errno = ENOSYS; /* not implemented */
+}
+
+/** Restores signal dispositions to what they were when bigshell was invoked
+ *
+ * @returns 0 on success, -1 on failure
+ *
+ */
+
+int
+signal_restore(void)
+{
+  /* TODO restore old actions 
+   *
+   * e.g. sigaction(SIGNUM, &saved_old_handler, NULL);
+   *
+   * */
+   // Changes all signals back to their old actions
+   if (sigaction(SIGINT, &old_sigint, NULL) < 0 || sigaction(SIGTTOU, &old_sigttou, NULL) < 0 || sigaction(SIGTSTP, &old_sigtstp, NULL) < 0) {
+    return -1;
+  }
+  //errno = ENOSYS; /* not implemented */
+  return 0;
+  
+}
